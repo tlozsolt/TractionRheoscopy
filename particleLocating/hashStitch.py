be698f172c39089fa5616ae525b769a0e69e7c95
zsolt <zsolt@seas.harvard.edu>
2020-03-01 19:33:48 -0500

    added hashStitch classes. There should be no or insignificant changes to dplHash

--- particleLocating/hashStitch.py
+++ particleLocating/hashStitch.py
@@ -29,67 +29,134 @@ Simple: if there is an existing nonzero pixel value, avg the values
 Complicated: compute the nnb hashValue and implement a specified matching function for the overlap region 
 """
 
-def getHashNNB(hashValue,rasterParam):
+class hashStitch:
     """
-    This a complicated function that will return the nnb hashValues for a speicfied hashValue. It handles all edge
-    cases and whether the material is gel or sediment and if those need to be stitched different. That is to say, the
-    output vector of nnb hash value has three types of value: number, different material, or edge.
-
-    This is complicated discrete function of the rasterParam (how many x and y chunks in plane and how many z chunks total)
-    but note that it is translation invariant and there is a lot of symmetry in the edge cases.
-
-    Actually, who cares about "computing" this function when it is **almost** tabluated when the hash was computed.
-    To get the table, I just need to keep track of the dummy indices used to count when cropping in x, y, and z.
-    These will be (i,j,k) values and the nnb are just (i+1,j,k),(i-1,j,k),(i, j+1,k),... etc
-    I also probably only need the 6 face sharing nnb but maybe this should an option.
-
-    returns a 26-tuple of nnb hashes with the possiblity that some entries are empty if the hashValue is an edge case
-    :param hashValue:
-    :return:
+    A class for creating stitched images that are outputed from hashed decon particle locating image processing
     """
+    def __init__(self,yamlPath):
+        # initalize the dplInstance associated with the yamlPath
+        self.dplInst = dpl.dplHash(yamlPath)
+        self.metaData = self.dplInst.metaData
+        # initialize the stitched image with three options: sed gel or both
+        self.imageParamDict = self.metaData['imageParam']
+        (gelBottom,sedGelInt,sedTop) = (0,self.imageParamDict['gelSedimentLocation'],self.imageParamDict['zDim'])
+        (xDim,yDim) = (self.imageParamDict['xDim'],self.imageParamDict['yDim'])
+        self.sedCanvas = (np.zeros((sedTop - sedGelInt,yDim, xDim),dtype=np.float64),\
+                          np.zeros((sedTop - sedGelInt,yDim, xDim),dtype=int)\
+                          )
+        self.gelCanvas = (np.zeros((sedGelInt,yDim, xDim),dtype=np.float64), \
+                          np.zeros((sedGelInt,yDim, xDim),dtype=int) \
+                          )
+        self.allCanvas = (np.zeros((sedTop,yDim, xDim),dtype=np.float64), \
+                          np.zeros((sedTop,yDim, xDim),dtype=int) \
+                          )
+        # I think we need a new data construct that is pair of arrays for each canvas.
+        # The canvas includes an array of stitched img values (ie just the stitched image)
+        # and an array that counts the number times that pixel has been updated.
+        # and maybe also a function that describes how the values are merged:
+        # average, replacement, max, or linear weight in overlap region.
+        # I think this could be implemented as some kind of discrete convolution on the counting matrix.
+        # just like averaging is a convolution with a kernel [0.3,0.3,0.3]
 
-def readLog(hashValue):
-    """
-    Read the log file for a given hashValue
-    return a dictionary of the pipeline steps as keys
-    """
+    def openImage(self,hv,step='postDecon', computer = 'ODSY'):
+        fPath = self.dplInst.getPath2File(hv,kwrd=step,computer=computer)
+        return flatField.zStack2Mem(fPath)
 
+    def integrateTransVect(self,hv,step='postDecon',computer='ODSY'):
+        """
+        Reads the yaml log file and returns the integrated translation vector for that specific hashValue
+        :param hv:  hashValue
+        :param step: step in pipeline that we should integrate up through. This should be done by searching the log \\
+                     file and not by reading the pipeline from yaml as, in principle the pipeline from yaml could
+                     reflect a job that was restarted.
+        :param computer:
+        :return: tuple giving global (x,y,z) of hash chunk origin.
+        """
+        # open the yaml file and parse it
+        with open(self.dplInst.getPath2File(hv,kwrd='log',computer=computer),'r') as stream:
+            yamlLog = yaml.load(stream,Loader=yaml.SafeLoader)
+        pipeline = self.dplInst.metaData['pipeline']
+        # create a list of pipeline steps that were both flagged as true and would be included in the log
+        keyList = [list(elt.keys())[0] for elt in pipeline]
+        bool = [list(elt.values())[0] for elt in pipeline]
+        pipelineSteps = [keyList[n] for n in range(len(keyList)) if bool[n]==True and keyList[n]!='rawTiff']
+        origin = np.array([0,0,0])
+        dim = np.array([0,0,0])
+        # I need to integrate up through step listed in arguement as a safegaurd against incomplete jobs.
+        for key in pipelineSteps:
+          if key != step:
+            try:
+              logEntry = yamlLog[key]
+              origin += logEntry['origin'] # note, relies on type conversion of logEntry to numpy.array
+              dim += logEntry['dim']
+            except KeyError:
+              print("There is something wrong when comparing pipeline to log. Perhaps incomplete job?")
+              raise KeyError
+          else: # break out of the for loop after you hit step kwrd arguement
+              origin += logEntry['origin'] # note, relies on type conversion of logEntry to numpy.array
+              dim += logEntry['dim']
+              break
+        return (origin,dim)
 
-def loadImage():
-    """ read image from fileName path into numpy array
-        return the array
-    """
-    return True
+    def hashStitch(self,subCanvas,hashImg):
+        """
+        Stitches hashImg onto subCanvas and averages values if multiple hashes overlap
+        This function does not do **any** translation and assume subcanvas has the same dimensions
+        as imArray.
+        At a single pixel this function updates the pixel to be:
+            subCanvas[i,j,k] = (count[i,j,k]*subCanvas[i,j,k] + hashImg[i,j,k])/(count[i,j,k] + 1)
+        :param subCanvas: pair of numpy arrays of image (dtype = float32) and imgCounter (dtype=int) \
+                       where imgCounter just counts how many times that pixel has been updated.
+        :param hashImg: the image array, likely 8bit that you would like to add to the canvas
+        :return: subCanvas with imArray included.
+        """
+        # Check dtypes and dimensions
+        if subCanvas[0].shape != hashImg.shape:
+            print("hashStitch called but subCanvas and hashImg have different dimensions:")
+            print("subCanvas[0].shape: ", str(subCanvas[0].shape))
+            print("subCanvas[1].shape: ", str(subCanvas[1].shape))
+            print("hashImg.shape: ", str(hashImg.shape))
+            raise IndexError
+        # apply the averaging function, if possible w/o a for loop
+        subCanvas = ((subCanvas[1][:]*subCanvas[0][:] + hashImg[:])/(subCanvas[1][:] + 1),\
+                     subCanvas[1][:] + 1
+                     )
+        return subCanvas
+        # return the subcanvas datatype.
 
-def getTranslationVector(hashValue, step = "postDecon"):
-    """
-    for a given hashValue, get the translation vector after specified processing step
-    note that the translation vector can in principle depend on any cropping done on the image
-    during image processing...in fact the cropping only needs to keep track of the updated translation vector
-    as the image dimensions can be determined on the fly.
-
-    :param hashValue: number of hashValue
-    :return: translation vector (x,y,z) to add to (0,0,0) coordinate of numpy array
-    """
-    return True
-
-def missingHV():
-    """
-    returns True if no missing hashValues; otherwise returns a list of missing hashValues.
-    Flow control is such that I am going to call one, possibly batch, job to stitch and reslice **all** the
-    stacks even if that sacrifices time in which I am waiting for a single hashValue to complete while all other
-    jobs are done.
-    :return:
-    """
-    return False
-
-def initStitchedImage():
-    """
-    initiliaze a numpy array with the correct dimensions for the final stitched stack
-    options and control:
-        - gel, sed, or all
-        - default dtype should be 8bit tiff
-    :return: initialized array
-    """
-
+    def placeHash(self,hashValue,step='postDecon',computer='ODSY',canvasType = 'all'):
+        """
+        Takes an image array and places
+        :param hashValue:
+        :param step: string describing what pipeline step to stitch
+        :param computer: string giving the computer, either 'MBP' or 'ODSY'
+        :param canvas: string specifying if you want to place Hash on 'sed;, 'gel', or 'all'.
+        :return:
+        """
+        # get the integrated origin and dim
+        origin,dim = self.integrateTransVect(hashValue,step=step,computer=computer)
+        imageHash = self.openImage(hashValue,step=step,computer=computer)
+        # decide on whether you want to placeHash on sed, gel, or both based on the canvas arguement
+        if canvasType =='all':
+            # careful to check xyz or zyx listing. I think origin, dim is xyz while allStitch in zyx indexing.
+            # cut out the subCanvas using origin and dim
+            subCanvas = [self.allCanvas[i][origin[2]:origin[2] + dim[2],\
+                                           origin[1]:origin[1] + dim[1],\
+                                           origin[0]:origin[0] + dim[0]]\
+                         for i in range(len(self.allCanvas))]
+            # apply hashStitch
+            subCanvas = self.hashStitch(subCanvas,imageHash)
+            # place the subCanvas back in using origin and dim
+            for i in range(len(self.allCanvas)):
+                self.allCanvas[i][origin[2]:origin[2] + dim[2],\
+                                  origin[1]:origin[1] + dim[1],\
+                                  origin[0]:origin[0] + dim[0]]\
+                = subCanvas[i]
+            # return the full canvas
+            return self.allCanvas
+        elif canvasType != 'all':
+            print("canvasType is: ", str(canvasType))
+            print("Stitching on sed or gel seperately has not yet been implemented")
+            raise TypeError
+        else: pass
 
